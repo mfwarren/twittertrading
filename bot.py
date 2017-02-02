3e46005a93187251c74296bb1611eb6e1d5b6c64
Matt Warren <matt.warren@gmail.com>
2017-02-01 21:07:25 -0500

    streaming tweets from users

--- bot.py
+++ bot.py
@@ -9,23 +9,41 @@ import os
 
 import tweepy
 
+consumer_key = os.environ['TWITTER_CONSUMER_KEY']
+consumer_secret = os.environ['TWITTER_CONSUMER_SECRET']
+access_token = os.environ['TWITTER_ACCESS_TOKEN']
+access_token_secret = os.environ['TWITTER_ACCESS_TOKEN_SECRET']
 
 FOLLOWING = {
-    '@POTUS': 822215679726100480,
-    '@realDonaldTrump': 25073877
+    '@POTUS': '822215679726100480',
+    '@realDonaldTrump': '25073877',
+    '@matt_warren': '14304643'
 }
+
 
 
 class MyStreamListener(tweepy.StreamListener):
 
     def on_status(self, status):
-        print(status.text)
+        if status.user.id_str in FOLLOWING.values():
+            print(status.text)
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+    def on_error(self, status_code):
+        if status_code == 403:
+            print("The request is understood, but it has been refused or access is not allowed. Limit is maybe reached")
+            return False
 
-api = tweepy.API(auth)
 
-myStreamListener = MyStreamListener()
-myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener())
-myStream.filter(follow='POTUS')
+def main():
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.secure = True
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True, retry_count=10, retry_delay=5, retry_errors=5)
+
+    myStreamListener = MyStreamListener()
+    myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
+    myStream.filter(follow=FOLLOWING.values(), async=True)
+
+
+if __name__ == '__main__':
+    main()
